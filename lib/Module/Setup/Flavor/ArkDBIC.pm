1c1c863b87710c50678afe533871526e8fcf1058
Masayuki Matsuki <y.songmu@gmail.com>
2011-10-27 19:35:10 +0900

    initial commit

---
+++ lib/Module/Setup/Flavor/ArkDBIC.pm
@@ -0,0 +1,32 @@
+package Module::Setup::Flavor::ArkDBIC;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Module::Setup::Flavor::ArkDBIC -
+
+=head1 SYNOPSIS
+
+  use Module::Setup::Flavor::ArkDBIC;
+
+=head1 DESCRIPTION
+
+Module::Setup::Flavor::ArkDBIC is
+
+=head1 AUTHOR
+
+Masayuki Matsuki E<lt>y.songmu@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
