27d80807e6486ceb855d1d271e6d2ff5c1e4695d
Masayuki Matsuki <y.songmu@gmail.com>
2011-10-28 21:05:20 +0900

    remove lines

--- lib/Module/Setup/Flavor/ArkDBIC.pm
+++ lib/Module/Setup/Flavor/ArkDBIC.pm
@@ -14,26 +14,11 @@ __END__
 
 =head1 NAME
 
-Module::Setup::Flavor::ArkDBIC -
+Module::Setup::Flavor::ArkDBIC - Ark flavor
 
 =head1 SYNOPSIS
 
   use Module::Setup::Flavor::ArkDBIC;
-
-=head1 DESCRIPTION
-
-Module::Setup::Flavor::ArkDBIC is
-
-=head1 AUTHOR
-
-Masayuki Matsuki E<lt>y.songmu@gmail.comE<gt>
-
-=head1 SEE ALSO
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
 
 =cut
 
